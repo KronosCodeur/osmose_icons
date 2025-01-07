f715b66d2eb7bea6ecfd53a79d9308e611c9336f
KronosCodeur <piraliarris@gmail.com>
2025-01-07 23:18:52 +0000

    update icons

--- lib/res/font_res.dart
+++ lib/res/font_res.dart
@@ -7,6 +7,6 @@ class FontRes {
   FontRes._();
 
   static const String PLUGIN_NAME = 'osmose';
-  static const String PLUGIN_VERSION = '0.0.1';
+  static const String PLUGIN_VERSION = '0.0.2';
   static const String ICOMOON = 'icomoon';
 }
